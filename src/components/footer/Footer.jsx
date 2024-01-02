3ea3652ca2e5729d25068af92339c13dea27d299
Arnab Kumar Das <78913766+Arnab-batsy@users.noreply.github.com>
2024-01-03 01:14:14 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
                     <div className="mb-6 md:mb-0">
                         <Link to="/" className="flex items-center">
                             <img
-                                src="/src/assets/mus.png"
+                                src="../../src/assets/mus.png"
                                 className="mr-3 h-28"
                                 alt="Logo"
                             />
