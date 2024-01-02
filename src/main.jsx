c905b748666927069070876e5a47980cf15d438b
Arnab Kumar Das <78913766+Arnab-batsy@users.noreply.github.com>
2024-01-02 23:09:20 +0530

    Fix users

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
-import Home from './components/home/Home.jsx'
-import About from './components/about/About.jsx'
-import Contact from './components/contact/contact.jsx'
-import User from './components/user/user.jsx'
-import Github from './components/github/github.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Home from "./components/home/Home.jsx";
+import About from "./components/about/About.jsx";
+import Contact from "./components/contact/contact.jsx";
+import User from "./components/user/user.jsx";
+import Github from "./components/github/github.jsx";
 
 //Here, we pass a array of objects that specify the path and the elements to render in that path
 //Add children to specify the other routes
@@ -34,22 +39,25 @@ import Github from './components/github/github.jsx'
 // ])
 
 //Another Method
-const router= createBrowserRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App/>}>
-      <Route path='' element={<Home/>}/>
-      <Route path='about' element={<About/>}/>
-      <Route path='contact' element={<Contact/>}/>
-      <Route path='user/:id' element={<User/>}/>
-      <Route path='github' element={<Github/>}/>
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user" element={<User />} >
+        <Route path=":id" element={<User />}/>
+      </Route>
+      <Route path="github" element={<Github />} />
     </Route>
   )
 );
 //Anything after the : is important to note in the Route path. That is the parameter passed.
 //params can be accessed by the element in the Route for ex- here it's User.
+//Loader fetches data from the API before useEffects whenever you hover to the button and stores it in cache.
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
